86ed4ed1c4940236c750758c34cf673986f13425
Antonio Riccelli <anton.riccelli@gmail.com>
2022-01-11 15:33:06 +0000

    previewer now renders initial example text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import Previewer from './Components/Previewer/index';
 
 const initialRendering = `# Hello There
 ## Hello
+[Google](https://www.google.com)
+Heres some code, \`<div></div>\`, between 2 __backticks__.
 
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+- to-do item
+> Block Quotes!
+![weird medieval image](https://64.media.tumblr.com/513dcf6a91fc4b6feb2bd71b5dff1856/tumblr_npjeybLLfI1qdm7pqo1_500.jpg)
 `
 
 
